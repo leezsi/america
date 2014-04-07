b7c4c36dfc86318b6392e65235e4598d05121bcd
hslavich <hernan.slavich@gmail.com>
2014-04-07 20:30:59 -0300

    Se elimina contructor de GameException

--- src/main/java/ar/edu/unq/americana/exceptions/GameException.java
+++ src/main/java/ar/edu/unq/americana/exceptions/GameException.java
@@ -9,23 +9,14 @@ public class GameException extends RuntimeException {
 
 	public GameException() {
 		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public GameException(final String message, final Throwable cause,
-			final boolean enableSuppression, final boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 
 	public GameException(final String message, final Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public GameException(final Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public GameException(final String description) {
