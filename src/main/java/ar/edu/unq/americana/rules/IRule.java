fcf982573d677f589972153be7a3333e6d108668
leezsi <leezsi@hotmail.com>
2014-05-06 19:07:20 -0300

    *andaaaaaa

--- src/main/java/ar/edu/unq/americana/rules/IRule.java
+++ src/main/java/ar/edu/unq/americana/rules/IRule.java
@@ -1,17 +1,7 @@
 package ar.edu.unq.americana.rules;
 
-
 public interface IRule<T, R> {
 	boolean mustApply(T component, R scene);
 
 	void apply(T component, R scene);
-}
-package ar.edu.unq.americana.rules;
-
-import ar.edu.unq.americana.utils.Vector2D;
-
-public interface IRule<T, R> {
-	boolean mustApply(T component, Vector2D nextPosition, R scene);
-
-	void apply(T component, Vector2D nextPosition, R scene);
 }
